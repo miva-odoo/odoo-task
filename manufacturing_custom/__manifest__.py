2f8d061ff0cabbf79984356e7b4773467920f13d
miva-odoo <miva@odoo.com>
2022-03-08 16:23:39 +0530

    [Development] manufacturing_start_sto__custom

---
+++ manufacturing_custom/__manifest__.py
@@ -0,0 +1,25 @@
+{
+#imfromation
+    
+ 'name':'Manufecturing Custom',
+ 'version': '15.0',
+ 'summary': 'Manufecturing Custom',
+ 'description': 'Manufecturing Custom',
+ 'category': 'custom',
+
+# Author
+ 'author': 'odoo Ps',
+ 'website': 'https://www.odoo.com/',
+ 'license': '',
+
+# Dependency
+ 'depends': ['mrp'],
+ 'data': [
+     'views/mrp_workorder_view.xml',
+     
+ ],
+
+ # Other
+ 'installable': True,
+ 'auto_install': False,
+}
