2f2bd88cdc2c15519991f9f8865f54ba4a45ab4e
miva-odoo <miva@odoo.com>
2022-03-09 14:30:38 +0530

    [Modify] Rename Module manufacturing_customization

---
+++ manufacturing_customization/__manifest__.py
@@ -0,0 +1,25 @@
+{
+#imfromation
+    
+ 'name':'Manufecturing Customization',
+ 'version': '15.0',
+ 'summary': 'Manufecturing Customization',
+ 'description': 'Manufecturing Customization',
+ 'category': 'custom',
+
+# Author
+ 'author': 'odoo Ps',
+ 'website': 'https://www.odoo.com/',
+ 'license': '',
+
+# Dependency
+ 'depends': ['mrp'],
+ 'data': [
+     'views/mrp_workorder_view.xml',
+     
+ ],
+
+ # Other
+ 'installable': True,
+ 'auto_install': False,
+}
