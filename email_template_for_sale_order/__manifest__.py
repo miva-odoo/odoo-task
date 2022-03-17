dc4172a328afc7bc303b7e0c8aab6db9b39cd02c
miva-odoo <miva@odoo.com>
2022-03-17 15:53:50 +0530

    [Development] email_template_for_sale_order module

---
+++ email_template_for_sale_order/__manifest__.py
@@ -0,0 +1,26 @@
+{
+#information
+  
+ 'name':'Email Template For Sale Order',
+ 'version': '15.0',
+ 'summary': 'Email Template For Sale Order',
+ 'description': 'Email Template For Sale Order',
+ 'category': 'custom',
+
+# Author
+ 'author': 'odoo Ps',
+ 'website': 'https://www.odoo.com/',
+ 'license': '',
+
+# Dependency
+ 'depends': ['sale_management'],
+ 'data': [
+     
+     'data/email_template_data.xml',
+    
+ ],
+
+ # Other
+ 'installable': True,
+ 'auto_install': False,
+}
